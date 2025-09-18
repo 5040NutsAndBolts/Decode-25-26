9cfcee6edeabecea5a580e0ad0d384f8312c4ee8
CoolPerson1237 <144965803+CoolPerson1237@users.noreply.github.com>
2025-09-18 13:21:52 -0400

    added slight tuning feature to FlyWheelOuttake
    might work might not

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/prototyping/FlywheelOuttake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/prototyping/FlywheelOuttake.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name="FlywheelOuttakePrototype", group="Teleop")
 public class FlywheelOuttake extends OpMode {
 	private DcMotor motor;
-
+	private double speed = 0.5;
 	@Override
 	public void init() {
 		motor = hardwareMap.get(DcMotor.class, "motor");
@@ -15,7 +15,16 @@ public class FlywheelOuttake extends OpMode {
 
 	@Override
 	public void loop() {
-		motor.setPower(gamepad1.left_stick_y);
+		
+		if(gamepad1.a && speed <= 1)
+			speed += 0.1;
+		if(gamepad1.b && speed >= 0)
+			speed -= 0.1;
+		while(gamepad1.left_stick_y > 0.1)
+			motor.setPower(speed);
+		while(gamepad1.left_stick_y < -0.1)
+			motor.setPower(-speed);
+		
 		telemetry.addLine("Motor Power: " + motor.getPower());
 		telemetry.update();
 	}
